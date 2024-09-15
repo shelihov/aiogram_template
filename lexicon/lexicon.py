94d848898ba5672e4f761ca4cbf90381f94702a1
schetelih <109992109+schetelih@users.noreply.github.com>
2024-09-15 22:55:07 +0300

    create set menu

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -4,3 +4,10 @@ LEXICON_RU: dict[str, str] = {
     'yes_button': 'Давай!',
     'no_button': 'Не хочу!'
 }
+
+LEXICON_COMMANDS_RU: dict[str, str] = {
+    '/command_1': 'command_1 desription',
+    '/command_2': 'command_2 desription',
+    '/command_3': 'command_3 desription',
+    '/command_4': 'command_4 desription'
+}
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from config_data.config import Config, load_config
 from handlers import other_handlers, user_handlers
+from keyboards.set_menu import set_main_menu
 
 logger = logging.getLogger(__name__)
 
@@ -29,6 +30,9 @@ async def main():
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
     dp = Dispatcher()
+
+    # Настраиваем кнопку Menu
+    await set_main_menu(bot)
 
     # Регистриуем роутеры в диспетчере
     dp.include_router(user_handlers.router)
